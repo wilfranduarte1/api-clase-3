e91ce0b9508a783dea8aec3fe60fc0e3ccbb980e
osm3rr <osm3rr@mail.com>
2025-02-22 17:46:58 -0400

    general permissions

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
 
@@ -10,3 +10,4 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAdminUser,)
