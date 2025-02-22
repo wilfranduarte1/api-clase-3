903f99562cabcd26dca276efc232195b141d31ba
osm3rr <osm3rr@mail.com>
2025-02-22 18:04:29 -0400

    curtom permissions

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,15 @@
 from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (IsAuthorOrReadOnly,)
